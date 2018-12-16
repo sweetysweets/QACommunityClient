a9756d7fe4f96f6fd3a2587d65e193c3c0e570c8
alwaysbest <106316276@qq.com>
2018-12-17 01:22:02 +0800

    跳转没毛病

--- pages/askquestion/askquestion.js
+++ pages/askquestion/askquestion.js
@@ -1,4 +1,6 @@
 // pages/askQuestion/askquestion.js
+const app = getApp()
+var util = require('../../utils/util.js')
 Page({
 
   /**
@@ -7,7 +9,53 @@ Page({
   data: {
 
   },
+  titleInput:function(e){
+    this.setData({
+      mytitle:e.detail.value
+    })
+  },
+  contentInput:function(e){
+    this.setData({
+      mycontent:e.detail.value
+    })
+  },
+  gotoList:function(options){
+    if(this.data.mytitle==null){
+      wx.showToast({
+        title:"问题必须大于4个字符",
+        icon:"none",
+        duration:3000
+      })
+    }else{
+      var mytime =util.formatTime(new Date);
 
+      // var uid = app.globalData.userInfo.uid;
+      console.log("title:" + this.data.mytitle);
+      console.log("content:"+this.data.mycontent);
+      console.log("time:"+mytime)
+      wx.request({
+        url: app.globalData.urlPath + 'submitquestion?userid=1&title=' + this.data.mytitle + '&content=' + this.data.mycontent+'&time='+mytime+'&state=0',
+        method:'POST',
+        success:function(res){
+          console.log("res"+res.data);
+          wx.showToast({
+            title: "已成功发布问题"
+          });
+          wx.switchTab({
+            url: '../index/index',
+          });
+          console.log("呵呵");
+        },
+        fail: function () {
+          console.log("data:"+data);
+          console.log("index.js wx.request CheckCallUser fail");
+        },
+        complete: function () {
+          // complete
+        }
+      })
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -63,4 +111,5 @@ Page({
   onShareAppMessage: function () {
 
   }
+ 
 })
