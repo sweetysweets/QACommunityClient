64d095c550214c5a1a13c04c89746e8737d11cd4
alwaysbest <106316276@qq.com>
2018-12-20 19:27:23 +0800

    单选框改成蓝色了

--- pages/askquestion/askquestion.js
+++ pages/askquestion/askquestion.js
@@ -75,14 +75,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-<<<<<<< HEAD
-=======
-
-    var that = this;
-    // that.data.user_id= app.globalData.userinfo.uid;
-
-    console.log(app.globalData.userInfo.id);
->>>>>>> origin/master
   },
 
   /**
